597902a73b17a2d933d4171dc617abead01ca9ae
SaudAnjum <saudjkstar@gmail.com>
2019-03-26 21:28:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OrderComponent } from './order/order.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavbarComponent
+  },
+  {
+    path: 'orders', component: OrderComponent, outlet: 'divOne'
+  },
+  {
+    path: 'products', component: ProductsComponent, outlet: 'divTwo'
+  },
+  {
+    path: 'checkOut', component: CheckOutComponent, outlet: 'divThree'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
